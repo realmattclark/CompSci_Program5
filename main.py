eb04dba236efbc4c26a8ab889ce78fd7253582b7
realmattclark <matt.clark5991@gmail.com>
2021-11-21 23:43:56 -0600

    main.py update

--- main.py
+++ main.py
@@ -1,31 +1,48 @@
-if __name__ == '__main__':
 
-    year = int(input('Enter the Year Your House was Built: '))
 
-    price = int(input('Enter the Initial Cost of House: '))
+while True:
 
-    location = input('Enter Location of House: ')
+    if __name__ == '__main__':
 
-class House:
+        year = int(input('Enter the Year Your House was Built: '))
 
-    def __init__(self, year, price, location):
-        self.year = year
-        self.price = price
-        self.location = location
+        price = int(input('Enter the Initial Cost of House: '))
 
-    def price(self):
-        return self.price
-    def year(self):
-        return year
-    def location(self):
-        return location
-    def current():
-        value = price * (1 + .08)^2021 - year
-        return value
+        location = input('Enter Location of House: ')
 
-    def __str__(self):
-        print('House Information: \nYear Built: {}\nPurchase Price: {}\nCurrent Value: {}\nLocation: {}'.format(year, price, current, location))
+        current = 0
 
-house = House(year, price, location)
+    class House:
 
-print(house)
+        def __init__(self, year, price, location, current):
+            self.year = str(year)
+            self.price = str(price)
+            self.location = location
+            self.current = current
+
+        def price(self):
+            return self.price
+        def year(self):
+            return year
+        def location(self):
+            return location
+        def current(self):
+            price*(1 + .08)**2021-year
+            return current
+
+        def __str__(self):
+            pass
+    house = House(year, price, location, current)
+
+    print('House Information: ')
+    print('Year Built: {}'.format(year))
+    print('Initial Price: {}'.format(price))
+    print('Location: {}'.format(location))
+    print('Current Value: {}'.format(current))
+    
+    choice = input('Enter Y/y to enter another home or N/n to quit')
+    if choice == 'y' or 'Y':
+        continue
+    elif choice == 'n' or 'N':
+        break
+
