8311fd4f9d5b29a6204cf17868d9a95a52279372
realmattclark <matt.clark5991@gmail.com>
2021-11-21 20:38:22 -0600

    update main.py

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
 
+if __name__ == '__main__':
+    print('Welcome to the House Calculator')
 
-class House:
-    year = 0
-    price = 0
-    location = 0
-    current = 0
-    def __init__(self, year, price, location, current):
-        self.year = year
-        self.price = price
-        self.location = location
-        self.current = current
+    class House:
+        
+        current = 0
+        def __init__(self, year, price, location, current):
+            self.year = year
+            self.price = price
+            self.location = location
+            self.current = current
     
-    def year(self):
-        self.year_built = int(input('Enter year house was built: '))
-        return year_built
+        def year(self):
+            self.year_built = int(input('Enter year house was built: '))
+            return year_built
     
-    def price(self):
-        self.first_price = int(input('Enter initial cost of house: '))
-        return first_price
+        def price(self):
+            self.first_price = int(input('Enter initial cost of house: '))
+            return first_price
     
-    def location(self):
-        self.home_location = input('Enter location of the house: ')
-        return home_location
+        def location(self):
+            self.home_location = input('Enter location of the house: ')
+            return home_location
     
-    def current_val(self):
-        first_price * (1 + .08)^(2021 - year_built)
-        return current_val
+        def current_val(self):
+            self.first_price * (1 + .08)^(2021 - year_built)
+            return current_val
     
-    def money_earned():
-        current_val - first_price
+        def money_earned(self):
+            self.money_earned = current_val - first_price
+            return money_earned
     
-    def __str__(self):
-        return 'House Information: \nYear Built: {}\nPurchase Price: {}\nCurrent Value: {}\nLocation: {}'.format(year_built, first_price, current_val, home_location)
+        def __str__(self):
+            return 'House Information: \nYear Built: {}\nPurchase Price: {}\nCurrent Value: {}\nLocation: {}'.format(year_built, first_price, current_val, home_location)
+    c1 = House(year = 1958, price = 100000, location = 'Kansas City', current = 0)
